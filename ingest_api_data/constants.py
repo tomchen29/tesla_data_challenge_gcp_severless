acd9af1c35f174888ed1ad2bc5c1bd65bd870574
Tonghong Chen <tonghong.july@gmail.com>
2020-11-26 23:34:23 -0500

    update

--- ingest_api_data/constants.py
+++
@@ -1,4 +0,0 @@
-TOKEN = 'ce561cf9d788fa8675889deb772a1327'
-PROJECT_ID = "tesla-iot-challenge"
-PUBSUB_TOPIC_NAME = "Telemetry_Ingestion"
-PUBSUB_TOPIC = f"projects/{PROJECT_ID}/topics/{PUBSUB_TOPIC_NAME}"
